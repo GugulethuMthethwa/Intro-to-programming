d80e9bfe27ae3e2a83b2f24ad20665f4bc4b096f
GugulethuMthethwa <gugulethumthethwa2508@gmail.com>
2024-07-28 15:14:20 +0200

    sixth commit

---
+++ Chapter 6/Chapter6.js
@@ -0,0 +1,32 @@
+ /*let name = "Nicholas";
+ console.log("Hello " + name);
+ if (name.length > 7) {
+ console.log("Wow, you have a REALLY long name!");
+}*/
+
+// changing if…else Statements
+
+/*let lemonChicken = false;
+let beefWithBlackBean = true;
+let sweetAndSourPork = true;
+
+if (lemonChicken) {
+ console.log("Great! I'm having lemon chicken!");
+} else if (beefWithBlackBean) {
+ console.log("I'm having the beef.");
+} else if (sweetAndSourPork) {
+ console.log("OK, I'll have the pork.");
+} else {
+ console.log("Well, I guess I'll have rice then.");
+}*/
+
+// Loops
+
+// While loops
+
+let sheepCounted = 0;
+ while (sheepCounted < 10) {
+ console.log("I have counted " + sheepCounted + " sheep!");
+ sheepCounted++;
+}
+console.log("Zzzzzzzzzzz");
