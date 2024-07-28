303fa2390010abf1fb2108549bad0c713cceb13b
GugulethuMthethwa <gugulethumthethwa2508@gmail.com>
2024-07-28 20:39:01 +0200

    seventh commit

---
+++ Chapter 7/Chapter7.js
@@ -0,0 +1,13 @@
+/* Creating a Prompt
+
+let name = prompt("What's your name?");
+console.log("Hello " + name);*/
+
+//Using confirm to Ask a Yes or No Question
+
+let likesCats = confirm("Do you like cats?");
+if (likesCats) {
+ console.log("You're a cool cat!");
+} else {
+ console.log("Yeah, that's fine. You're still cool!");
+}
